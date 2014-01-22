0bc670c7ce573e8a453bd3f4feb5f3710b372ed0
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-22 02:27:12 +0200

    unimportant

--- luajit.cmd
+++ luajit.cmd
@@ -1,5 +1,4 @@
-@echo off
-rem execute luajit from this directory so that media files are found using relative paths.
-pushd %~dp0
-bin\mingw32\luajit %*
-popd
+@rem luajit wrapper that starts luajit in this directory so that scripts can access their files using relative paths.
+@pushd %~dp0
+@call bin\mingw32\luajit %*
+@popd
