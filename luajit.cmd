3722b113c471ed0bd1be91a8185893df583e2caf
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-27 01:47:31 +0200

    unimportant

--- luajit.cmd
+++ luajit.cmd
@@ -1,4 +1,4 @@
 @rem luajit wrapper that starts luajit in this directory so that scripts can access their files using relative paths.
-@pushd %~dp0
+@pushd "%~dp0"
 @call bin\mingw32\luajit %*
 @popd
