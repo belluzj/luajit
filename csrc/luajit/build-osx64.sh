b62418e2cb934812f04db7b8d57525fdc4300ab1
Cosmin <cosmin@Mac.local>
2014-01-31 03:35:09 +0200

    osx64 build

---
+++ csrc/luajit/build-osx64.sh
@@ -0,0 +1,20 @@
+# TODO: use `make amalg PREFIX=` and `make install DESTDIR=`
+set -e
+#git clone http://luajit.org/git/luajit-2.0.git src
+cd src
+#git checkout -b build v2.0.2
+cd src
+make clean
+make
+
+bindir=../../../../bin/osx64
+platform_dir="$bindir/lua"
+cp -f luajit "$bindir/luajit-bin"
+cp -f libluajit.so "$bindir/"
+mkdir -p "$platform_dir/jit"
+cp -Rf jit/*.lua "$platform_dir/jit/"
+
+make clean
+
+cd ../..
+#rm -rf luajit-2.0
